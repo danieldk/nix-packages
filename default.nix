95467f05d09d3cda06b1bc3f56bac9e3a36ce34c
Daniël de Kok <me@danieldk.eu>
2019-05-17 20:48:48 +0200

    fsa6: init at 280

--- default.nix
+++ default.nix
@@ -1,5 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
+  # Homegrown software
   citar = pkgs.callPackage ./citar {};
+
+  # NLP utilities
+  fsa6 = pkgs.callPackage ./fsa6 {};
 }
---
+++ fsa6/default.nix
@@ -0,0 +1,32 @@
+{ stdenv, fetchurl, autoPatchelfHook, makeWrapper, tcl-8_5, tk-8_5 }:
+
+stdenv.mkDerivation rec {
+  name = "fsa6-${version}";
+  version = "280";
+
+  src = fetchurl {
+    url = "https://www.let.rug.nl/~vannoord/Fsa/binaries/fsa6-${version}-x86_64-linux-glibc2.19.tar.gz";
+    sha256 = "1b140b9d31bc1a504893ad17b59893e89e2140ea943e7642e9b4efe616f46093";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
+  buildInputs = [ tcl-8_5 tk-8_5 ];
+
+  dontStrip = true;
+
+  buildPhase = ":";
+
+  installPhase = ''
+    mkdir -p $out/libexec/fsa6
+    cp -a * $out/libexec/fsa6
+
+    mkdir $out/bin
+    makeWrapper $out/libexec/fsa6/binary/create/fsa $out/bin/fsa6 --prefix TCLLIBPATH ' ' "${tk-8_5}/lib"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Finite state automata utilities";
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+  };
+}
