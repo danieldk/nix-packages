57f64697ad623284c541bfba96d4ce3e22b04e5a
Daniël de Kok <me@danieldk.eu>
2019-05-18 10:02:21 +0200

    dact: init at 2.6.6

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
-{
+rec {
   # Homegrown software
   alpinocorpus = pkgs.callPackage ./alpinocorpus {};
   citar = pkgs.callPackage ./citar {};
+  dact = pkgs.callPackage ./dact { alpinocorpus = alpinocorpus; };
 
   # NLP utilities
   fsa6 = pkgs.callPackage ./fsa6 {};
