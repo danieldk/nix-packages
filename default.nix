041b806cd681939e860cd11cd02e776c42c6690c
Daniël de Kok <me@danieldk.eu>
2019-05-28 15:54:41 +0200

    pythonPackages.finalfusion: init at 0.2.0

--- default.nix
+++ default.nix
@@ -8,6 +8,34 @@ rec {
   dact = pkgs.callPackage ./dact { alpinocorpus = alpinocorpus; };
   sticker = pkgs.callPackage ./sticker { libtensorflow = libtensorflow_1_13_1; };
 
+  pythonPackages = python2Packages;
+  python2Packages = python27Packages;
+  python3Packages = python37Packages;
+
+  python27Packages = {
+    finalfusion = pkgs.callPackage ./python-modules/finalfusion {
+      pythonPackages = pkgs.python27Packages;
+    };
+  };
+
+  python35Packages = {
+    finalfusion = pkgs.callPackage ./python-modules/finalfusion {
+      pythonPackages = pkgs.python35Packages;
+    };
+  };
+
+  python36Packages = {
+    finalfusion = pkgs.callPackage ./python-modules/finalfusion {
+      pythonPackages = pkgs.python36Packages;
+    };
+  };
+
+  python37Packages = {
+    finalfusion = pkgs.callPackage ./python-modules/finalfusion {
+      pythonPackages = pkgs.python37Packages;
+    };
+  };
+
   # NLP utilities
   fsa6 = pkgs.callPackage ./fsa6 {};
 
---
+++ python-modules/finalfusion/default.nix
@@ -0,0 +1,59 @@
+{ pkgs, stdenv, fetchFromGitHub, makeRustPlatform, pythonPackages }:
+
+let
+  nixpkgs_unstable = import (builtins.fetchTarball {
+    name = "nixpkgs-stable-2019-05-18";
+    url = https://github.com/nixos/nixpkgs/archive/2a56ea3593aa0702a61313572c2211ef947f4d1f.tar.gz;
+    sha256 = "05iggc96f3nmylmdgys8i53kd7vfh546lkyi87qqms6vq8v2fybm";
+  }) {};
+  mozillaOverlay = fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
+    sha256 = "18h0nvh55b5an4gmlgfbvwbyqj91bklf1zymis6lbdh75571qaz0";
+  };
+  mozilla = import "${mozillaOverlay.out}/package-set.nix" { inherit pkgs; };
+  rustNightly = (mozilla.rustChannelOf { date = "2019-02-07"; channel = "nightly"; }).rust;
+  rustPlatform = nixpkgs_unstable.makeRustPlatform {
+    cargo = rustNightly;
+    rustc = rustNightly;
+  };
+in
+  rustPlatform.buildRustPackage rec {
+    pname = "finalfusion";
+    version = "0.2.0";
+
+    src = fetchFromGitHub {
+      owner = "finalfusion";
+      repo = "finalfusion-python";
+      rev = "1501d5c2d0e3f561fc82d228dcca0df19ad36d3a";
+      # Use in future versions:
+      #rev = version;
+      sha256 = "080pc2x4gvvzl3f4i8k4grhr9s8xrkjlcjfh2f3ivszrlv6ii6jv";
+    };
+
+    cargoSha256 = "0xzhggxfap80i6daa7hdafszmdvizv93glr2cqhkzpx3y5qh4hwd";
+
+    nativeBuildInputs = [ nixpkgs_unstable.pyo3-pack ];
+
+    buildInputs = [ pythonPackages.python ];
+
+    propagatedBuildInputs = [ pythonPackages.numpy ];
+
+    buildPhase = ''
+      pyo3-pack build --release --manylinux off
+    '';
+
+    installPhase = ''
+      ${pythonPackages.python.pythonForBuild.pkgs.bootstrapped-pip}/bin/pip install \
+        target/wheels/*.whl --no-index --prefix=$out --no-cache --build tmpbuild
+    '';
+
+    doCheck = false;
+
+    meta = with stdenv.lib; {
+      description = "Python module for the finalfusion embedding format";
+      license = licenses.asl20;
+      platforms = platforms.all;
+    };
+  }
