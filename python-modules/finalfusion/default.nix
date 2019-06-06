4737e9ba72f7a4a6bb282e3f063fd510db5c5a44
Daniël de Kok <me@danieldk.eu>
2019-06-06 09:56:45 +0200

    pythonPackages.finalfusion: replace import by callPackage

--- python-modules/finalfusion/default.nix
+++ python-modules/finalfusion/default.nix
@@ -12,7 +12,7 @@ let
     rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
     sha256 = "18h0nvh55b5an4gmlgfbvwbyqj91bklf1zymis6lbdh75571qaz0";
   };
-  mozilla = import "${mozillaOverlay.out}/package-set.nix" { inherit pkgs; };
+  mozilla = pkgs.callPackage "${mozillaOverlay.out}/package-set.nix" { };
   rustNightly = (mozilla.rustChannelOf { date = "2019-02-07"; channel = "nightly"; }).rust;
   rustPlatform = nixpkgs_unstable.makeRustPlatform {
     cargo = rustNightly;
