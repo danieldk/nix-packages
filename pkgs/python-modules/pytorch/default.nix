95f2ca2c5e1e22be0ebf06a06dfcf2d5bf8bdf82
Daniël de Kok <me@danieldk.eu>
2019-12-06 08:51:57 +0100

    python3Packages.pytorch.v1_3_1: init at 1.3.1
    Copy PyTorch derivation from nixpkgs and bump the version to
    1.3.1. This allows us to maintain our own PyTorch versions
    out-of-band.

---
+++ pkgs/python-modules/pytorch/default.nix
@@ -0,0 +1,10 @@
+{ callPackage, lib, cudaSupport, magma }:
+
+let
+  createDerivation = { version, sha256 }: {
+    # Convert a version such as "1.3.1" to the attribute "v1_3_1".
+    name = "v${builtins.replaceStrings ["."] ["_"] version}";
+    value = callPackage ./generic.nix { inherit version sha256 cudaSupport magma; };
+  };
+  versions = import ./versions.nix;
+in builtins.listToAttrs (map createDerivation versions)
---
+++ pkgs/python-modules/pytorch/versions.nix
@@ -0,0 +1,6 @@
+[
+  {
+    version = "1.3.1";
+    sha256 = "07ga9806c5q02hsdybwdw9nc0r1a8dinwjakyg6wlrccclc027lq";
+  }
+]
