8d0ec2c7cd48c96072694f465b61ad7ed59cb074
Daniël de Kok <me@danieldk.eu>
2019-12-06 08:51:57 +0100

    python3Packages.pytorch.v1_3_1: init at 1.3.1
    Copy PyTorch derivation from nixpkgs and bump the version to
    1.3.1. This allows us to maintain our own PyTorch versions
    out-of-band.

--- pkgs/python-modules/default.nix
+++ pkgs/python-modules/default.nix
@@ -1,4 +1,4 @@
-{ callPackage, alpinocorpus, python }:
+{ pkgs, callPackage, alpinocorpus, python }:
 
 let
   disabledIf = x: drv:
@@ -8,6 +8,16 @@ in {
 
   alpinocorpus = callPackage ./alpinocorpus {
     inherit alpinocorpus;
+  };
+
+  pytorch = callPackage ./pytorch {
+    cudaSupport = pkgs.config.cudaSupport or false;
+
+    magma = pkgs.magma.overrideAttrs (attrs: rec {
+      preConfigure = ''
+        export CC=${pkgs.cudatoolkit.cc}/bin/gcc CXX=${pkgs.cudatoolkit.cc}/bin/g++
+      '';
+    });
   };
 
   somajo = callPackage ./somajo {};
