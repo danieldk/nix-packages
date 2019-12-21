6ee2e3d0e1de65e8ced67fb7e6e844a3319242f4
Daniël de Kok <me@danieldk.eu>
2019-12-21 09:17:26 +0100

    python3Packages.somajo: 1.11.0 -> 2.0.1

--- pkgs/python-modules/somajo/default.nix
+++ pkgs/python-modules/somajo/default.nix
@@ -2,14 +2,14 @@
 
 buildPythonPackage rec {
   pname = "somajo";
-  version = "1.11.0";
+  version = "2.0.1";
   disabled = !isPy3k;
 
   src = fetchFromGitHub {
     owner = "tsproisl";
     repo = "SoMaJo";
     rev = "v${version}";
-    sha256 = "0wj5fyzszfiwhi2l4ygkb18mxwj1l9j2yn5l5jnas50f0yd33rsd";
+    sha256 = "1841z74h9mv7k8xywncsk1vn90nlxsfx6ipv5aydf9fw9hyipgsv";
   };
 
   propagatedBuildInputs = [ regex ];
