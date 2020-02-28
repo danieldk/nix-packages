5ab9ea454558689f9cf55b68460228ba1b1067c4
Daniël de Kok <me@danieldk.eu>
2020-02-28 11:12:18 +0100

    python3Packages.somajo: 2.0.1 -> 2.0.3

--- pkgs/python-modules/somajo/default.nix
+++ pkgs/python-modules/somajo/default.nix
@@ -1,22 +1,23 @@
 { pkgs, stdenv, fetchFromGitHub, buildPythonPackage, isPy3k, regex }:
 
 buildPythonPackage rec {
-  pname = "somajo";
-  version = "2.0.1";
+  pname = "SoMaJo";
+  version = "2.0.3";
   disabled = !isPy3k;
 
   src = fetchFromGitHub {
     owner = "tsproisl";
-    repo = "SoMaJo";
+    repo = pname;
     rev = "v${version}";
-    sha256 = "1841z74h9mv7k8xywncsk1vn90nlxsfx6ipv5aydf9fw9hyipgsv";
+    sha256 = "1js06jsjyddsd22546fvhcgiz848kvynfwh2fxjgnazzw9344hji";
   };
 
   propagatedBuildInputs = [ regex ];
 
   meta = with stdenv.lib; {
     description = "Tokenizer and sentence splitter for German and English web texts";
+    homepage = "https://github.com/tsproisl/SoMaJo";
     license = licenses.gpl3;
-    platforms = platforms.all;
+    maintainers = with maintainers; [ danieldk ];
   };
 }
