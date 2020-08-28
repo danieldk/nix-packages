6da9b92838418526d159f78e298fd670b8d6c097
Daniël de Kok <me@danieldk.eu>
2020-08-28 15:10:17 +0200

    python3Packages.alpino-tokenizer: 0.1.0 -> 0.1.1

--- pkgs/python-modules/alpino-tokenizer/default.nix
+++ pkgs/python-modules/alpino-tokenizer/default.nix
@@ -1,49 +1,44 @@
 { stdenv
 , callPackage
-, fetchgit
+, fetchFromGitHub
 
   # Native build inputs
 , maturin
-, rustNightly
+, pipInstallHook
+, rustPlatform
 
   # Build inputs
 , python
 }:
 
-(rustNightly "2020-01-20").buildRustPackage rec {
+rustPlatform.buildRustPackage rec {
   pname = "alpino-tokenizer";
   version = "0.1.1";
   name = "${python.libPrefix}-${pname}-${version}";
 
-  src = fetchgit {
-    url = "https://git.sr.ht/~danieldk/alpino-tokenizer-python";
+  src = fetchFromGitHub {
+    owner = "danieldk";
+    repo = "alpino-tokenizer-python";
     rev = version;
-    sha256 = "14f345wcqbkfrxxifs64b9686sn5g8sxlr7zsvncq2zqykcasspq";
+    sha256 = "0ly9xsfrv094qqw3n6kjwzhqh382y2gwms5qw0gc9x0b19jjxgz6";
   };
 
-  cargoSha256 = "0fipz79wnly8mj76m5dl9rzyzzdadsnxdgd538pz4zphfw0hzscv";
+  cargoSha256 = "1cjqwhibbbkfzk0nh0zdq0dscp1l9mn39b14l4wycjibhfa3nlcv";
 
-  nativeBuildInputs = [ maturin python.pkgs.pip ];
+  nativeBuildInputs = [ maturin pipInstallHook ];
 
-  buildInputs = [ python ];
+  propagatedBuildInputs = [ python ];
 
   doCheck = false;
 
-    buildPhase = ''
-    runHook preBuild
-
+  buildPhase = ''
     maturin build --release --manylinux off
-
-    runHook postBuild
   '';
 
   installPhase = ''
-    runHook preInstall
-
-    ${python.pythonForBuild.pkgs.bootstrapped-pip}/bin/pip install \
-      target/wheels/*.whl --no-index --prefix=$out --no-cache --build tmpbuild
-
-    runHook postInstall
+    # Put the wheels where the pip install hook can find them.
+    install -Dm644 -t dist target/wheels/*.whl
+    pipInstallPhase
   '';
 
   meta = with stdenv.lib; {
--- pkgs/python-modules/default.nix
+++ pkgs/python-modules/default.nix
@@ -1,12 +1,10 @@
-{ pkgs, callPackage, alpinocorpus, python, rustNightly }:
+{ pkgs, callPackage, alpinocorpus, python }:
 
 let
   disabledIf = x: drv:
     if x then throw "${drv.pname or drv.name} not supported for interpreter ${python.executable}" else drv;
 in {
-  alpino-tokenizer = disabledIf (!python.isPy3k) (callPackage ./alpino-tokenizer {
-    inherit rustNightly;
-  });
+  alpino-tokenizer = disabledIf (!python.isPy3k) (callPackage ./alpino-tokenizer {});
 
   alpinocorpus = callPackage ./alpinocorpus {
     inherit alpinocorpus;
